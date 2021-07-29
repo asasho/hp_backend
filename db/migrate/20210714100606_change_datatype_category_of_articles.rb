19173a529d4db1c026163cbe7ca0e20036537f08
asasho <cy.asano4@gmail.com>
2021-07-29 13:03:40 +0000

    a

--- db/migrate/20210714100606_change_datatype_category_of_articles.rb
+++ db/migrate/20210714100606_change_datatype_category_of_articles.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeCategoryOfArticles < ActiveRecord::Migration[6.0]
   def change
-    change_column :articles, :category, :integer
+    change_column :articles, :category, 'integer USING CAST(category AS integer)'
   end
 end
