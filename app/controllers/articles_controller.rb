9efdf0f3c9cc6fab85681e2dec653dd306eb3e0d
asasho <cy.asano4@gmail.com>
2021-07-30 12:45:50 +0000

    change_image_link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,8 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+    render json: @articles.to_json(methods: [:image_url])
   end
-  
-  def new
-    @article = Article.new
-  end
-  
-  def create
-  end
-  
-  def edit
-    @article = Article.find_by(id: params[:id])
-  end
-  
-  def update
-    @article = Article.find_by(id: params[:id])
-    if @article.update(article_params)
-      redirect_to "/admin/articles"
-    else
-      render 'edit'
-    end
-  end
-  
   # def create 
   #   @article = Article.new(article_params)
   #   respond_to do |format|
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < ApplicationRecord
+    include Rails.application.routes.url_helpers
+    
    has_one_attached :thumbnail
+   
+   
+   def image_url
+       thumbnail.attached? ? url_for(thumbnail) : nil
+   end
 end
