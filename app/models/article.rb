ab58ddb266a3effe18fb8e38d94315ade9b550ab
asasho <cy.asano4@gmail.com>
2021-08-03 14:52:29 +0000

    change_storage

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,12 @@
 class Article < ApplicationRecord
     include Rails.application.routes.url_helpers
-    
+    default_scope -> { order(updated_at: :asc) }
    has_one_attached :thumbnail
    
    enum category: { プレスリリース: 0, メディア: 1, その他: 2 }
+   
+   validates :thumbnail,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                      message: "must be a valid image format" }
    
    
    def image_url
